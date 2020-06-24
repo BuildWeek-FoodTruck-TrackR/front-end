644320dc14bbdc167ff8fe0b7568b3754eefcbdc
kevin thew <kevthew@outlook.com>
2020-06-23 19:35:38 -0600

    added style to sign up form, created SignUpFormApp.js, updated sign up form link in  log in form

---
+++ foodtruck/src/Components/signUpFormApp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import SignUpForm from "./Components/SignUpForm";
+
+function SignUpApp() {
+  return (
+    <div className="SignUpApp">
+      <SignUpForm />
+    </div>
+  );
+}
+
+export default SignUpApp;
